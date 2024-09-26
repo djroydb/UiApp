d6d903dcd00c2d4a12c20e6db7d8408e7a2bc363
djroydb <djroydb@gmail.com>
2024-09-26 19:31:28 -0300

    Primeiro commit

---
+++ UiApp/UiApp/UiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiAppApp.swift
+//  UiApp
+//
+//  Created by Robson Freitas on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct UiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
